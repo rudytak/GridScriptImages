4b6079b69b3551d624f5847efba4c283d4d36d65
rudytak <75335232+rudytak@users.noreply.github.com>
2021-05-17 18:01:05 +0200

    Initial commit

---
+++ Script02/sketch.js
@@ -0,0 +1,26 @@
+const img_count = 4;
+var imgs = [];
+const cellSize = 100;
+
+function preload() {
+    for (var i = 1; i <= img_count; i++) {
+        var name = "000" + i;
+        name = name.substring(name.length - 3);
+        imgs.push(loadImage(name + '.png'));
+    }
+}
+
+function setup() {
+    createCanvas(imgs[0].width, imgs[0].height);
+}
+
+function draw() {
+    for (var x = 0; x < width; x += cellSize) {
+        for (var y = 0; y < height; y += cellSize) {
+            image(imgs[(x / cellSize + y / cellSize) % img_count], x, y, cellSize, cellSize, x, y, cellSize, cellSize)
+        }
+    }
+
+    noLoop();
+    saveCanvas("file", "png")
+}
---
+++ Script04/sketch.js
@@ -0,0 +1,27 @@
+const img_count = 4;
+var sequence = [1, 2, 3, 4, 4, 4, 4]
+var imgs = [];
+const cellSize = 100;
+
+function preload() {
+    for (var i = 1; i <= img_count; i++) {
+        var name = "000" + i;
+        name = name.substring(name.length - 3);
+        imgs.push(loadImage(name + '.png'));
+    }
+}
+
+function setup() {
+    createCanvas(imgs[0].width, imgs[0].height);
+}
+
+function draw() {
+    for (var x = 0; x < width; x += cellSize) {
+        for (var y = 0; y < height; y += cellSize) {
+            image(imgs[sequence[(x / cellSize + y / cellSize) % sequence.length] - 1], x, y, cellSize, cellSize, x, y, cellSize, cellSize)
+        }
+    }
+
+    noLoop();
+    saveCanvas("file", "png")
+}
